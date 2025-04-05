549fc5db8cec806e8cabb0f95a0cc164aff64fc1
Aziz <azizrasiodv99@gmail.com>
2025-04-05 21:48:57 +0500

    Add implement  MarkerStyle class

--- Flyweight_Pattern/MarkerStyle.java
+++ Flyweight_Pattern/MarkerStyle.java
@@ -1,4 +1,26 @@
 package Flyweight_Pattern;
 
 public class MarkerStyle {
+    private String color;
+    private String icon;
+    private String style;
+
+    public MarkerStyle(String color, String icon, String style) {
+        this.color = color;
+        this.icon = icon;
+        this.style = style;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
 }
